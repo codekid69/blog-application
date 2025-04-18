dc7a0a65cb50f757df9eebaf7330fd2fabee1b88
Yash Bisht <bishtyash069@gmail.com>
2025-04-18 19:56:05 +0530

    Added

--- index.js
+++ index.js
@@ -1,37 +1,44 @@
-const express =require('express');
-const app=express();
-const path= require('path');
-const port=process.env.PORT||8000;
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 8000;
 const cookieParser = require('cookie-parser');
-const userRoute=require('./routes/user');
-const blogRoute=require('./routes/blog');
-const staticRoutes=require('./routes/staticRoutes.js')
-const db=require('./config/db');
+const userRoute = require('./routes/user');
+const blogRoute = require('./routes/blog');
+const staticRoutes = require('./routes/staticRoutes.js')
+const db = require('./config/db');
 require('dotenv').config();
-const {checkForAuthenticationCookie,requireAuth} = require('./middlewares/authentication');
+const fs = require('fs');
+// Create uploads folder if it doesn't exist
+const uploadsDir = path.join(__dirname, 'public/uploads');
+if (!fs.existsSync(uploadsDir)) {
+    fs.mkdirSync(uploadsDir, { recursive: true });
+    console.log('📁 uploads folder created');
+}
+const { checkForAuthenticationCookie, requireAuth } = require('./middlewares/authentication');
 
 
-db().then(()=>{
+db().then(() => {
     console.log("connected to the database");
-}).catch((error)=>{
+}).catch((error) => {
     console.log(`Error while Connecting to the database ${error}`);
 })
 
 
 app.use(cookieParser());
 app.use(checkForAuthenticationCookie("token"));
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }))
 app.use('/images', express.static(path.join(__dirname, 'public/images')));
 app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
 
-app.set('view engine','ejs');
-app.set('views',path.resolve('./views'));
+app.set('view engine', 'ejs');
+app.set('views', path.resolve('./views'));
 
 
-app.use('/user',userRoute);
-app.use('/blog',blogRoute)
-app.use('/',staticRoutes)
+app.use('/user', userRoute);
+app.use('/blog', blogRoute)
+app.use('/', staticRoutes)
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server is runnig on ${port}`);
 })
