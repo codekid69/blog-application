52adcb4fb7657c57647039cf1e847aa1bcaa6a81
codekid69 <bishtyash069@gmail.com>
2025-06-21 11:08:53 +0530

    fixed prod issue

--- routes/chat.js
+++ routes/chat.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Message = require("../models/message");
-const User = require("../models/User");
+const User = require("../models/user");
 
 // Get chat page between two users
 router.get("/:userId", async (req, res) => {
